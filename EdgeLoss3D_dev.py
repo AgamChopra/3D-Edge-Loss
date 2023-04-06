85ad7d92a33ac4e6d9af8ec715de59cc5dca42b2
Agam_ubuntu_mac <achopra4@uw.edu>
2023-04-06 13:49:29 -0700

    documentation/comments added

--- EdgeLoss3D_dev.py
+++ EdgeLoss3D_dev.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jul 10 2022
+Last Modified on Thu Apr 6 2023
 
-@author: Agam Chopra, achopra4@uw.edu
+@author: Agamdeep Chopra, achopra4@uw.edu
 @affiliation: Department of Mechanical Engineering, University of Washington, Seattle WA
 @reference: Thevenot, A. (2022, February 17). Implement canny edge detection from scratch with PyTorch. Medium. Retrieved July 10, 2022, from https://towardsdatascience.com/implement-canny-edge-detection-from-scratch-with-pytorch-a1cccfa58bed 
 """
@@ -15,21 +16,47 @@ EPSILON = 1E-12
 
 
 def get_sobel_kernel3D(n1=1, n2=2, n3=2):
+    '''
+    Returns 3D Sobel kernels Sx, Sy, Sz, and diagonal kernels
+    ex: 
+        Sx = [[[-n1, 0, n1],
+               [-n2, 0, n2],
+               [-n1, 0, n1]],
+              [[-n2, 0, n2],
+               [-n2*n3, 0, n2*n3],
+               [-n2, 0, n2]],
+              [[-n1, 0, n1],
+               [-n2, 0, n2],
+               [-n1, 0, n1]]]
+    
+
+    Parameters
+    ----------
+    n1 : int, optional
+        kernel value 1. The default is 1.
+    n2 : int, optional
+        kernel value 2. The default is 2.
+    n3 : int, optional
+        kernel value 3. The default is 2.
+
+    Returns
+    -------
+    list
+        list of all the 3d sobel kernels.
+
+    '''
     Sx = np.asarray([[[-n1, 0, n1], [-n2, 0, n2], [-n1, 0, n1]], [[-n2, 0, n2],
                     [-n3*n2, 0, n3*n2], [-n2, 0, n2]], [[-n1, 0, n1], [-n2, 0, n2], [-n1, 0, n1]]])
     Sy = np.asarray([[[-n1, -n2, -n1], [0, 0, 0], [n1, n2, n1]], [[-n2, -n3*n2, -n2],
                     [0, 0, 0], [n2, n3*n2, n2]], [[-n1, -n2, -n1], [0, 0, 0], [n1, n2, n1]]])
     Sz = np.asarray([[[-n1, -n2, -n1], [-n2, -n3*n2, -n2], [-n1, -n2, -n1]],
                     [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[n1, n2, n1], [n2, n3*n2, n2], [n1, n2, n1]]])
-
     Sd11 = np.asarray([[[0, n1, n2], [-n1, 0, n1], [-n2, -n1, 0]], [[0, n2, n2*n3],
                       [-n2, 0, n2], [-n2*n3, -n2, 0]], [[0, n1, n2], [-n1, 0, n1], [-n2, -n1, 0]]])
     Sd12 = np.asarray([[[-n2, -n1, 0], [-n1, 0, n1], [0, n1, n2]], [[-n2*n3, -n2, 0],
                       [-n2, 0, n2], [0, n2, n2*n3]], [[-n2, -n1, 0], [-n1, 0, n1], [0, n1, n2]]])
-
     Sd21 = Sd11.T
     Sd22 = Sd12.T
-
     Sd31 = np.asarray([-S.T for S in Sd11.T])
     Sd32 = np.asarray([S.T for S in Sd12.T])
 
@@ -37,6 +64,9 @@ def get_sobel_kernel3D(n1=1, n2=2, n3=2):
 
 
 class GradEdge3D():
+    '''
+    Sobel edge detection algorithm compatible with PyTorch Autograd engine.
+    '''
     def __init__(self, n1=1, n2=2, n3=2, device='cpu'):
         super(GradEdge3D, self).__init__()
         self.device = device
@@ -53,6 +83,22 @@ class GradEdge3D():
             self.sobel_filters.append(sobel_filter)
 
     def detect(self, img, a=1):
+        '''
+        Detect edges using Sobel operator for a 3d image
+
+        Parameters
+        ----------
+        img : torch tensor
+            3D torch tensor of shape (b, c, x, y, z).
+        a : int, optional
+            padding to be added, do not change unless necessary. The default is 1.
+
+        Returns
+        -------
+        torch tensor
+            tensor of gradient edges of shape (b, 1, x, y, z).
+
+        '''
         pad = (a, a, a, a, a, a)
         B, C, H, W, D = img.shape
 
@@ -66,6 +112,9 @@ class GradEdge3D():
 
 
 class GMELoss3D(nn.Module):
+    '''
+    3D-Edge Loss for PyTorch with choice of criterion. Default is MSELoss.
+    '''
     def __init__(self, criterion=nn.MSELoss(), n1=1, n2=2, n3=2, device='cpu'):
         super(GMELoss3D, self).__init__()
         self.edge_filter = GradEdge3D(n1, n2, n3, device)
@@ -75,33 +124,25 @@ class GMELoss3D(nn.Module):
         y_edge = self.edge_filter.detect(y)
         yp_edge = self.edge_filter.detect(yp)
         error = self.criterion(y_edge, yp_edge)
-
         return error
 
 
 if __name__ == "__main__":
+    cmap = 'gray'
     loss = GMELoss3D()
     filter_ = GradEdge3D(n1=1, n2=2, n3=2)
+
     for k in range(1, 5):
-        # path = 'R:/img (%d).pkl' % (k)  # '/brats/data/img (%d).pkl'%(k
-        #data = np.load(path, allow_pickle=True)
-        # x = torch.from_numpy(data[0]).view(
-        #    1, 1, data[0].shape[0], data[0].shape[1], data[0].shape[2]).to(dtype=torch.float)
         x = torch.rand((1, 1, 150, 150, 20))
-        Y = filter_.detect(x)
-        print(Y.shape)
-        titles = ['grad_magnitude']
+        Y = [x, filter_.detect(x)]
+        print([y.shape for y in Y])
+        titles = ['input', 'grad_magnitude']
+
         for j in range(0, 20, 1):
             for i, y in enumerate(Y):
-                if i == 0:
-                    cmap = 'gray'
-                elif i == 4 or i == 5:
-                    cmap = 'bone'
-                elif i == 6:
-                    cmap = 'binary'
-                else:
-                    cmap = 'PiYG'
-                plt.imshow(y[0, :, :, j].squeeze(
+                plt.imshow(y[0, 0, :, :, j].squeeze(
                 ).cpu().detach().numpy().T, cmap=cmap)
-                plt.title(titles[i] + ' slice%d' % (j))
+                plt.title(titles[i] + ' slice %d' % (j))
                 plt.show()
+
+    print('test_loss =', loss(x, x + 0.001 * torch.rand((1, 1, 150, 150, 20))))
